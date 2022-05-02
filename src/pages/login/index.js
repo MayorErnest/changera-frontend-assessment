25e1a11fe1774ddc9628977996260992dd0f4b1a
Obumma Ernest <eobumma@gmail.com>
2022-05-02 09:55:37 +0100

    --feat: add login page

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,9 +1,18 @@
 import React from "react";
+import { GithubLogo } from "../../assets/svgs";
+import { Button } from "../../components";
 
 import styles from "./styles.module.css";
 
 const Login = () => {
-	return <main className={styles.container}>Login</main>;
+	return (
+		<main className={styles.container}>
+			<div className={styles.box}>
+				<GithubLogo />
+				<Button btnType="secondary">Authorize</Button>
+			</div>
+		</main>
+	);
 };
 
 export { Login };
