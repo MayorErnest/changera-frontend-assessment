5253e7f3cf021a1c03e014d3c44a5a8b5d1cfd79
Obumma Ernest <eobumma@gmail.com>
2022-05-03 06:32:11 +0100

    --fix: remove extra slash in github base url

--- src/store/slices/repoSlice/index.js
+++ src/store/slices/repoSlice/index.js
@@ -22,7 +22,7 @@ const { getRepos } = repoSlice.actions;
 export const fetchRepo = (userName, sortParameter) => async (dispatch) => {
 	axios
 		.get(
-			`${process.env.REACT_APP_GITHUB_BASE_USER_URL}//${userName}/repos?per_page=20&sort=${sortParameter}`
+			`${process.env.REACT_APP_GITHUB_BASE_USER_URL}/${userName}/repos?per_page=20&sort=${sortParameter}`
 		)
 		.then((data) => dispatch(getRepos(data?.data)))
 		.catch((error) => console.log(error));
